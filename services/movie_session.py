d9dbb7b505f5763fd98c4d579bfec074fafb5764
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-09-09 13:03:43 +0300

    feat: models and services base init (part2)

--- services/movie_session.py
+++ services/movie_session.py
@@ -42,3 +42,6 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+def get_taken_seats(movie_session_id : int) -> QuerySet:
+    pass
---
+++ services/order.py
@@ -0,0 +1,9 @@
+from django.db.models.query import QuerySet
+
+from db.models import Order
+
+def create_order() -> Order:
+    pass
+
+def get_orders() -> Order:
+    pass
